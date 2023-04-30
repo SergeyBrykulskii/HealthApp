a6a10823c6a686fbee983e9964a68186f80c8d7c
Sergey Brykulskii <serxio6758sk@gmail.com>
2023-04-30 22:39:57 +0300

    Добавил регистрацию сервисов в консольное приложение

--- HealthApp/ConsoleApp/Program.cs
+++ HealthApp/ConsoleApp/Program.cs
@@ -1,1 +1,29 @@
-﻿Console.WriteLine("Adfads");
+﻿using HealthApp.Application.Abstractions.ConsoleApp;
+using HealthApp.Application.Services.ConsoleApp;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using IHost host = CreateHostBuilder(args).Build();
+using var scope = host.Services.CreateScope();
+
+var services = scope.ServiceProvider;
+
+try
+{
+    services.GetRequiredService<App>.Run(args);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+static IHostBuilder CreateHostBuilder(string[] args)
+{
+    return Host.CreateDefaultBuilder(args)
+        .ConfigureServices((_, services) =>
+        {
+            services.AddSingleton<ICardService, CardService>();
+            services.AddSingleton<IDoctorService, DoctorService>();
+            services.AddSingleton<IPatientService, PatientService>();
+        });
+}
