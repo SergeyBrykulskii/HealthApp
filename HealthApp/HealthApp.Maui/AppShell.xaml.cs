62a64849ba7e95554fd207bdd31fbcdae4ce59b4
Sergey Brykulskii <serxio6758sk@gmail.com>
2023-05-15 10:28:29 +0300

    Сделал maui приложение

---
+++ HealthApp/HealthApp.Maui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HealthApp.Maui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ HealthApp/HealthApp.Maui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace HealthApp.Maui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
