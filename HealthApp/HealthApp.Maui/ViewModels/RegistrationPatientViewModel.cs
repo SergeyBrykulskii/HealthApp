f08eba1b32ffbfb0cf117dd63c88070d43f95f53
Sergey Brykulskii <serxio6758sk@gmail.com>
2023-05-24 01:29:46 +0300

    Регистрация для пациента

--- HealthApp/HealthApp.Maui/ViewModels/RegistrationPatientViewModel.cs
+++ HealthApp/HealthApp.Maui/ViewModels/RegistrationPatientViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using HealthApp.Application.Abstractions;
 using HealthApp.Domain.Entities;
 using HealthApp.Maui.Services.Abstractions;
@@ -11,12 +12,41 @@ public partial class RegistrationPatientViewModel : ObservableObject
     private readonly IPatientService _patientService;
 
     [ObservableProperty]
-    private Patient _patient;
+    private Patient _newPatient;
+
+    [ObservableProperty]
+    private string _password;
+
+    [ObservableProperty]
+    private string _confirmPassword;
 
     public RegistrationPatientViewModel(IPasswordService passwordService, IPatientService patientService)
     {
         _passwordService = passwordService;
         _patientService = patientService;
-        _patient = new Patient();
+        _newPatient = new Patient();
+    }
+
+    [RelayCommand]
+    public async Task Register()
+    {
+        OnPropertyChanged(nameof(Doctor));
+        if (NewPatient.Name is null || NewPatient.Email is null ||
+            NewPatient.Name == "" || NewPatient.Email == "")
+        {
+            await App.Current.MainPage.DisplayAlert("Error", "All fields are required", "Ok");
+            return;
+        }
+        if (_password != _confirmPassword)
+        {
+            await App.Current.MainPage.DisplayAlert("Error", "Passwords do not match", "Ok");
+            return;
+        }
+        NewPatient.Password = _passwordService.GetHashedPassword(_password);
+        await _patientService.AddAsync(NewPatient);
+        await _patientService.SaveAllAsync();
+
+        await App.Current.MainPage.DisplayAlert("Success", "You have successfully registered", "Ok");
+        await Shell.Current.GoToAsync("//LoginPage");
     }
 }
